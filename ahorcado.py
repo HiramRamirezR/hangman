96135f75e73bc2d1a71cce6c7f13af4706894e37
Hiram Ramírez <68158101+HiramRamirezR@users.noreply.github.com>
2025-07-30 21:23:24 -0600

    English translation

--- ahorcado.py
+++ ahorcado.py
@@ -1,6 +1,6 @@
-# si la letra es incorrecta, agregar una parte al cuerpo y reduce un intento
-# si la letra ya ha sido ingresada, mostrar un mensaje de error
-# comenzar el juego con 6 intentos
+# if the letter is incorrect, add a part to the body and reduce one attempt
+# if the letter has already been entered, show an error message
+# start the game with 6 attempts
 
 import random
 
@@ -87,17 +87,17 @@ letters = []
 attempts = 6
 spaces = ["_"] * len(word)
 
-print(f"Vamos a comenzar el juego.\nTu palabra tiene {len(word)} letras.\n")
+print(f"Let's start the game.\nYour word has {len(word)} letters.\n")
 print(person[0])
 print(f"{spaces}\n")
 
 
 while attempts > 0:
 
-    letter = input(f"Ingresa una letra. Tienes {attempts} intentos.\n")
+    letter = input(f"Enter a letter. You have {attempts} attempts left.\n")
 
     if letter in letters:
-        print(f"\n-------------------------------------\nYa has ingresado la letra '{letter}'.")
+        print(f"\n-------------------------------------\nYou have already entered the letter '{letter}'.")
         print(f"{spaces}\n")
         continue
 
@@ -110,18 +110,18 @@ while attempts > 0:
         for i, char in enumerate(word):
             if char == letter:
                 spaces[i] = letter
-        print("\n-------------------------------------\n!Bien hecho, sigue adelante! \n")
+        print("\n-------------------------------------\nWell done, keep going! \n")
 
     else:
-        print(f"\n-------------------------------------\nLa letra '{letter}' no está en la palabra.")
+        print(f"\n-------------------------------------\nThe letter '{letter}' is not in the word.")
         attempts -= 1
 
     print(person[6 - attempts])
     print(f"{spaces}\n")
 
     if "_" not in spaces:
-        print("¡Felicidades! Has adivinado la palabra correctamente.")
+        print("Congratulations! You have guessed the word correctly.")
         break
 
 if attempts == 0:
-    print(f"Fin del juego. La palabra era '{word}'.")
+    print(f"Game over. The word was '{word}'.")
